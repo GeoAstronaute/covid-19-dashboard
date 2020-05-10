3d675ad6b956327fdcbb65cad226f5ef50a145b9
GeoAstronaute <jeanmarc.royy@gmail.com>
2020-05-10 15:04:36 +0200

    feat(webmap): preserve extent on type swtich + add basemapGallery/legend widgets

--- src/app/webmap/webmap/webmap.component.ts
+++ src/app/webmap/webmap/webmap.component.ts
@@ -11,6 +11,7 @@ export class WebmapComponent implements OnInit {
   @Input() center: [number, number] = [0, 0];
   @Input() type: '2D'|'3D' = '3D';
   @Output() typeChange = new EventEmitter<'2D'|'3D'>();
+  @Output() mapInit = new EventEmitter<void>();
   view: __esri.MapView | __esri.SceneView;
   map: __esri.Map;
 
@@ -28,35 +29,71 @@ export class WebmapComponent implements OnInit {
     this.map = new Map({
       basemap: 'hybrid',
     });
-    this.type === '2D' ? this.buildMapView() : this.buildSceneView();
+    this.type === '2D' ? await this.buildMapView() : await this.buildSceneView();
+    this.mapInit.emit();
+    this.addWidgets(this.view);
   }
 
   async buildMapView() {
     const [MapView]: [
       __esri.MapViewConstructor
     ] = await loadModules(['esri/views/MapView']);
-    this.view = new MapView({
+    const viewProperties: __esri.MapViewProperties = {
       container: 'webmap',
       map: this.map,
       center: this.center,
       zoom: 2
-    });
+    };
+    if (this.view?.extent) {
+      viewProperties.center = this.view.center;
+      viewProperties.zoom = this.view.zoom;
+    }
+    this.view = new MapView(viewProperties);
   }
 
   async buildSceneView() {
     const [SceneView]: [
       __esri.SceneViewConstructor
     ] = await loadModules(['esri/views/SceneView']);
-    this.view = new SceneView({
+    const viewProperties: __esri.SceneViewProperties = {
       container: 'webmap',
       map: this.map,
       center: this.center,
       zoom: 2
-    });
+    };
+    if (this.view?.extent) {
+      viewProperties.center = this.view.center;
+      viewProperties.zoom = this.view.zoom;
+    }
+    this.view = new SceneView(viewProperties);
   }
 
-  updateView(type) {
+  async updateView(type) {
     this.typeChange.emit(type);
-    type === '2D' ? this.buildMapView() : this.buildSceneView();
+    type === '2D' ? await this.buildMapView() : await this.buildSceneView();
+    this.addWidgets(this.view);
+  }
+
+  async addWidgets(view: __esri.MapView | __esri.SceneView) {
+    const [BasemapGallery, Legend, Expand]: [
+      __esri.BasemapGalleryConstructor,
+      __esri.LegendConstructor,
+      __esri.ExpandConstructor
+    ] = await loadModules(['esri/widgets/BasemapGallery', 'esri/widgets/Legend', 'esri/widgets/Expand']);
+    const basemapGallery = new BasemapGallery({
+      view,
+      container: document.createElement('div')
+    });
+    const expandBasemapGallery = new Expand({
+      view,
+      content: basemapGallery
+    });
+    const legend = new Legend({view});
+    const expandLegend = new Expand({
+      view,
+      content: legend
+    });
+    view.ui.add(expandBasemapGallery, 'bottom-left');
+    view.ui.add(expandLegend, 'bottom-right');
   }
 }
