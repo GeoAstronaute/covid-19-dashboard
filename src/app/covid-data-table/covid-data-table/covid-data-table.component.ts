c747235e517c9b2bf015fd6217f2121eef899531
GeoAstronaute <jeanmarc.royy@gmail.com>
2020-05-13 10:32:23 +0200

    fix(CovidDataTable): clear subscription onDestroy

--- src/app/covid-data-table/covid-data-table/covid-data-table.component.ts
+++ src/app/covid-data-table/covid-data-table/covid-data-table.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewChild } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromRoot from '../../stores';
 import { selectCountry } from 'src/app/stores/country/country.actions';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
-import { filter, withLatestFrom, map } from 'rxjs/operators';
+import { filter, withLatestFrom, map, takeUntil } from 'rxjs/operators';
 import { Sort } from '@angular/material/sort';
-import { BehaviorSubject, combineLatest } from 'rxjs';
+import { BehaviorSubject, combineLatest, Subject } from 'rxjs';
 
 @Component({
   selector: 'co19-covid-data-table',
@@ -13,7 +13,7 @@ import { BehaviorSubject, combineLatest } from 'rxjs';
   styleUrls: ['./covid-data-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CovidDataTableComponent implements OnInit {
+export class CovidDataTableComponent implements OnInit, OnDestroy {
   @ViewChild(CdkVirtualScrollViewport) scrollViewport: CdkVirtualScrollViewport;
   sorting$ = new BehaviorSubject<Sort>({active: 'countryOrRegion', direction: 'asc'});
   latestData$ = combineLatest([this.store.select(fromRoot.selectLatestDataWithCountryInfo), this.sorting$.asObservable()])
@@ -30,13 +30,15 @@ export class CovidDataTableComponent implements OnInit {
       ({...d, selected: selectedCountryId && selectedCountryId === d.alpha2Code})
     ))
   );
+  componentDestroyed$ = new Subject<void>();
 
   constructor(private store: Store) { }
 
   ngOnInit(): void {
     this.selectedCountryId$.pipe(
       filter(id => !!id),
-      withLatestFrom(this.latestData$)
+      withLatestFrom(this.latestData$),
+      takeUntil(this.componentDestroyed$)
     ).subscribe(([id, data]) => {
       const index = data.findIndex(d => d.alpha2Code === id);
       if (index) {
@@ -59,6 +61,11 @@ export class CovidDataTableComponent implements OnInit {
 
   compare(a: number | string, b: number | string, isAsc: boolean) {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+
+  ngOnDestroy() {
+    this.componentDestroyed$.next();
+    this.componentDestroyed$.complete();
   }
 
 }
